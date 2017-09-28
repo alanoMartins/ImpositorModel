6d51b42292ebfb1be2bd3fe2d0a8f4ddc3ef3afd
Alano Martins Pinto <alanomartinspinto@gmail.com>
2017-09-28 01:22:05 -0300

    WIP acc

--- main_alt.py
+++ main_alt.py
@@ -8,6 +8,7 @@ import numpy as np
 import functools
 import math
 from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
 import random
 import decimal
 from numpy import linalg as LA
@@ -15,18 +16,17 @@ from numpy import linalg as LA
 
 
 Ng = 3
-#Nv = len(data[0])
 
-def kmeans_gauss(X):
-    return ([2,4,1], [5,1,2], [2,1 ,2])
 
 def gauss(x, u, e):
     D = len(x)
     det = abs(np.linalg.det(e))
-    det = round(det,3)
+    det = round(det, 3)
     p1 = 1 / (2 * math.pi)**(D/2) * (np.sqrt(det))
 
     sub = x - u
+    m = 10 ^ -6
+    e_aux = e + np.eye(e.shape[1])*m
     invE = np.linalg.inv(e)
     sub = sub.reshape(4, 1)
     aux1 = np.dot(sub.T, invE)
@@ -36,8 +36,10 @@ def gauss(x, u, e):
     p2 = round(p2, 3)
     return p1 * math.e ** p2
 
+
 def weighted_gauss(x, m, u, e):
     return m * gauss(x, u, e)
+
 
 def sum_weighted_gauss(x, Lam):
     acc = []
@@ -47,6 +49,7 @@ def sum_weighted_gauss(x, Lam):
         e_arr = Lam[2]
         acc.append(weighted_gauss(x, m_arr[g], u_arr[g], e_arr[g]))
     return sum(acc)
+
 
 def log_sum_p(X, lam):
     acc = []
@@ -59,14 +62,15 @@ def log_sum_p(X, lam):
 
 def GMM(X):
     l = kmeans_manual(X)
-    lam = max_EMM(X, l)
-    return (1 / len(X) * math.log(log_sum_p(X, lam)))
+    return max_EMM(X, l)
+
 
 def get_prob(X, lamb):
     acc = []
     for i in range(0, len(X)):
         acc.append(sum_weighted_gauss(X[i], lamb))
     return sum(acc)
+
 
 def max_EMM(X, lamb):
     max_interation = 10
@@ -75,19 +79,18 @@ def max_EMM(X, lamb):
         p_old = get_prob(X, lamb)
         lamb_new = EMM(X, lamb)
         p_new = get_prob(X, lamb_new)
-        if get_prob(X, lamb_new) > get_prob(X, lamb):
+        if p_new > p_old:
             lamb = lamb_new
+    return lamb
 
 
 def EMM(X, lam):
-    new_lam_m = []
-    new_lam_u = []
-    new_lam_e = []
+    m_arr = lam[0]
+    u_arr = lam[1]
+    e_arr = lam[2]
+
     Lgi = []
     for g in range(0, Ng):
-        m_arr = lam[0]
-        u_arr = lam[1]
-        e_arr = lam[2]
 
         lgg = []
         for i in range(0, len(X)):
@@ -96,34 +99,40 @@ def EMM(X, lam):
             lgg.append(aux1 / aux2)
         Lgi.append(lgg)
 
-    for g in range(0, Ng):
-        Lg = sum(Lgi[g])
-        mg = Lg / len(X)
 
+    Lg = []
+    lg = []
+    for g in range(0, Ng):
+        for i in range(0, len(X)):
+            lg.append(Lgi[g][i])
+        Lg.append(sum(lg))
+
+    Mg = []
+    for g in range(0, Ng):
+        mg = Lg[g] / len(X)
+        Mg.append(mg)
+
+    Ug = []
+    for g in range(0, Ng):
         ug_arr = []
         for i in range(0, len(X)):
             ug_arr.append(X[i] * Lgi[g][i])
 
-        ug = (1/Lg * sum(ug_arr))
+        ug = (1/Lg[g] * np.sum(ug_arr))
+        Ug.append(ug)
 
+    SigG = []
+    for g in range(0, Ng):
         sigG_arr = []
         for i in range(0, len(X)):
-            arr = X[i] - ug
+            arr = X[i] - Ug[g]
             outer_x = np.outer(arr, arr)
             sigG_arr.append(outer_x * Lgi[g][i])
-        sigG =  (1 / Lg) * sum(sigG_arr)
+        sigG = (1 / Lg[g]) * sum(sigG_arr)
         sigG = np.asmatrix(sigG)
+        SigG.append(sigG)
 
-        new_lam_m.append(mg)
-        new_lam_u.append(ug)
-        new_lam_e.append(sigG)
-
-    return (new_lam_m, new_lam_u, new_lam_e)
-
-# def kmeans(X):
-#     kmeans = KMeans(n_clusters=Ng, random_state=0).fit(X)
-#     print(kmeans.cluster_centers_)
-
+    return Mg, Ug, SigG
 
 
 def kmeans_manual(X):
@@ -132,7 +141,7 @@ def kmeans_manual(X):
     ng = []
     Yi = []
     for g in range(0, Ng):
-        ix = random.randint(0, Nv)
+        ix = random.randint(0, Nv - 1)
         Ug.append(X[ix])
     loop = 1
     endloop = 10
@@ -186,20 +195,81 @@ def kmeans_manual(X):
             egtemp.append(outer_prod * Yi[i])
         eg.append((1 / Ng) * sum(egtemp))
 
-
-
     return mg, Ug, eg
 
+def compare(X_test, y, models):
+    accepts = []
+    for ix in range(0, len(X_test)):
+        X = X_test[ix]
+        best_p = 0
+        best_i = -1
+        for m in range(0, len(models)):
+            p = sum_weighted_gauss(X, models[m])
+            if p > best_p:
+                best_p = p
+                best_i = m
+        res = y[ix]
+        accepts.append(best_i == res)
+
+    print(sum(accepts) / len(X_test))
+
+
+def cross_validation(dataset, target, test_size):
+    c0 = dataset[0:50]
+    c1 = dataset[50:100]
+    c2 = dataset[100:150]
+
+    t0 = target[0:50]
+    t1 = target[50:100]
+    t2 = target[100:150]
+
+    n_interations = round(len(c0) * test_size)
+    test_arr = []
+    test_tar = []
+
+    for i in range(0, n_interations):
+        rand_i = random.randint(0, len(c0) - 1)
+        test_arr.append(c0[rand_i])
+        test_tar.append(t0[rand_i])
+        c0 = np.delete(c0, rand_i, axis=0)
+        t0 = np.delete(t0, rand_i, axis=0)
+
+    for i in range(0, n_interations):
+        rand_i = random.randint(0, len(c1) - 1)
+        test_arr.append(c1[rand_i])
+        test_tar.append(t1[rand_i])
+        c1 = np.delete(c1, rand_i, axis=0)
+        t1 = np.delete(t1, rand_i, axis=0)
+
+    for i in range(0, n_interations):
+        rand_i = random.randint(0, len(c2) - 1)
+        test_arr.append(c2[rand_i])
+        test_tar.append(t2[rand_i])
+        c2 = np.delete(c2, rand_i, axis=0)
+        t2 = np.delete(t2, rand_i, axis=0)
+
+    return np.array([c0, c1, c2]), np.array([t0, t1, t2]), test_arr, test_tar
 
 def exec():
-    data, target = load_iris(True)
-    c0 = data[0:50]
-    c1 = data[50:100]
-    c2 = data[100:150]
 
-    d1 = np.array(c0)
-    print(c0)
-    r = GMM(d1)
+    data, target = load_iris(True)
+
+    X_train, y_train, X_test, y_test = cross_validation(data, target, 0.4)
+
+
+    c0, c1, c2 = X_train
+
+    orq1 = np.array(c0)
+    orq2 = np.array(c1)
+    orq3 = np.array(c2)
+
+    modelOrq1 = GMM(orq1)
+    modelOrq2 = GMM(orq2)
+    modelOrq3 = GMM(orq3)
+
+    models = [modelOrq1, modelOrq2, modelOrq3]
+    compare(X_test, y_test, models)
+
 
 
 exec()
